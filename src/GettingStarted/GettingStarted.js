00d32ba258ae1f13616fdcd1c9e6d692ba751f07
Caleb Ford <techeadred@icloud.com>
2021-03-16 11:13:31 -0700

    GettingStarted component init commit working static

---
+++ src/GettingStarted/GettingStarted.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class GettingStarted extends React.Component {
+    render() {
+        return (
+            <section className="main getstrt" id="getstrt">
+                <h1>Usful</h1>
+                <h2>Coronavirus resources are often filled with complex numbers and professional language that can be
+                    overwhelming to the general public. USFUL is designed to offer easy to digest data and
+                    information. The information hereon provided is meant as a resource and should not be treated as a
+                    substitute for personal medical attention, diagnosis, or hands-on treatment.
+                </h2>
+                <h4>If this is your first time visiting, and want to learn more about the site, click the button below. (Button is not working at this time)</h4>
+                <button>Learn More</button>
+            </section>
+        )
+    }
+}
+
+export default GettingStarted;
